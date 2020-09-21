d1e4086cad6ca14533ab892f86a72e4bf5057282
João Brito <macbook@MacBook-Air-de-Joao-Brito.local>
2020-09-21 16:35:09 +0100

    .

--- resources/views/pages/about.blade.php
+++ resources/views/pages/about.blade.php
@@ -3,5 +3,55 @@
 @section('title','About')
 
 @section('content')
-
+    <section class="sample-text-area">
+        <div class="container">
+            <h3 class="text-heading title_color">Filosofia</h3>
+            <p class="sample-text">
+                A chave para o nosso sucesso baseia-se na aposta em recursos humanos valiosos e aptos aos desafios do quotidiano. Os nossos valores assentam-se na filosofia japonesa do método Kaizene, que prima pelo melhoramento diário baseado sempre nos erros cometidos, para que estes sejam evitados.
+            </p>
+        </div>
+    </section>
+    <div class="whole-wrap">
+		<div class="container">
+            <div class="section-top-border">
+                <div class="row">
+                    <div class="col-md-4">
+                        <div class="single-defination">
+                            <h4 class="mb-20">Missão</h4>
+                            <p>Fornecer aos nossos clientes todo o material e equipamentos que se adequem às suas necessidades, contando com parecerias fortes nos ramos da construção, metalomecânicas, oficinas, material agrícola, ect. Preservando sempre a alta qualidade e o melhor serviço Pré e Pós-venda. </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="single-defination">
+                            <h4 class="mb-20">Visão</h4>
+                            <p>Ser uma empresa líder de mercado na nossa zona de atuação, primando sempre pela excelência na relação com todos os nossos clientes. </p>
+                        </div>
+                    </div>
+                    <div class="col-md-4">
+                        <div class="single-defination">
+                            <h4 class="mb-20">Valores</h4>
+                            <ul>
+                                <li>Honestidade</li>
+                                <li>Ética</li>
+                                <li>Qualidade </li>
+                                <li>Dedicação</li>
+                                <li>Responsabilidade social e ambiental</li>
+                            </ul>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="section-top-border">
+				<h3 class="mb-30 title_color">História</h3>
+				<div class="row">
+					<div class="col-lg-12">
+						<blockquote class="generic-blockquote">
+							Gabriel Antunes Unipessoal, Lda surge no mercado a 21 de Outubro de 2019, fundada pelo seu sócio gerente Jorge Antunes. Atualmente, após um ano de atividade, a Gabriel Antunes, Lda conta com mais de 510 clientes ativos e pretende continuar a crescer.<br>
+                            A Gabriel Antunes, Lda distingue-se das outras empresas pela excelência no atendimento e na atenção dada a cada cliente. A empresa conta com uma equipa com mais de 18 anos de experiência no setor, capaz de resolver os problemas mais complexos que os seus clientes apresentem.
+						</blockquote>
+					</div>
+				</div>
+			</div>
+        </div>
+    </div>
 @endsection
